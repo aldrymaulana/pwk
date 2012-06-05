68fb43945566ec866c716f5d474c19d180a1ed92
rofect@gmail.com <rofect@gmail.com@3a97358f-1fe1-9197-085c-5d56440e6970>
2012-06-05 06:34:17 +0000

    Patch System Redirect to login screen if user not login

--- trunk/application/controllers/admin/user.php
+++ trunk/application/controllers/admin/user.php
@@ -16,31 +16,34 @@ class User extends CI_Controller {
 //end Main
     //load Grid
     function index() {
-        $colModel['no'] = array('No', 30, TRUE, 'center', 0);
-        $colModel['Username'] = array('Username', 150, TRUE, 'center', 1);
-        $colModel['Edit'] = array('Edit', 30, TRUE, 'center', 0);
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
+        } else {
+            $colModel['no'] = array('No', 30, TRUE, 'center', 0);
+            $colModel['Username'] = array('Username', 150, TRUE, 'center', 1);
+            $colModel['Edit'] = array('Edit', 30, TRUE, 'center', 0);
 
-        $gridParams = array(
-            'width' => 'auto',
-            'height' => 300,
-            'rp' => 10,
-            'rpOptions' => '[5,10,15,20,25,40]',
-            'pagestat' => 'Menampilkan: {from} hingga {to} dari {total} data.',
-            'blockOpacity' => 0.5,
-            'title' => 'Daftar User',
-            'showTableToggleBtn' => true
-        );
+            $gridParams = array(
+                'width' => 'auto',
+                'height' => 300,
+                'rp' => 10,
+                'rpOptions' => '[5,10,15,20,25,40]',
+                'pagestat' => 'Menampilkan: {from} hingga {to} dari {total} data.',
+                'blockOpacity' => 0.5,
+                'title' => 'Daftar User',
+                'showTableToggleBtn' => true
+            );
 
-        $buttons[] = array('tambah', 'add', 'spt_js');
-        $buttons[] = array('separator');
-        $buttons[] = array('hapus', 'delete', 'spt_js');
+            $buttons[] = array('tambah', 'add', 'spt_js');
+            $buttons[] = array('separator');
+            $buttons[] = array('hapus', 'delete', 'spt_js');
 
 
-        // mengambil data dari file controler ajax pada method grid_berkas
-        $grid_js = build_grid_js('flex1', site_url("admin/user/grid_user"), $colModel, 'Username', 'asc', $gridParams, $buttons);
+            // mengambil data dari file controler ajax pada method grid_berkas
+            $grid_js = build_grid_js('flex1', site_url("admin/user/grid_user"), $colModel, 'Username', 'asc', $gridParams, $buttons);
 
-        $data['added_js'] =
-                "<script type='text/javascript'>
+            $data['added_js'] =
+                    "<script type='text/javascript'>
 		function spt_js(com,grid)
 		{
 			if (com=='tambah')
@@ -76,9 +79,10 @@ class User extends CI_Controller {
 		";
 
 
-        $data['js_grid'] = $grid_js;
-        $data['content'] = $this->load->view('admin/grid', $data, true);
-        $this->load->view('admin/main', $data);
+            $data['js_grid'] = $grid_js;
+            $data['content'] = $this->load->view('admin/grid', $data, true);
+            $this->load->view('admin/main', $data);
+        }
     }
 
     function grid_user() {
@@ -105,74 +109,91 @@ class User extends CI_Controller {
     }
 
     function form() {
-        $data['status'] = 'new';
-        $data['failed'] = false;
-        $data['aa'] = '';
-        $data['content'] = $this->load->view('admin/form_user', $data, true);
-        $this->load->view('admin/main', $data);
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
+        } else {
+            $data['status'] = 'new';
+            $data['failed'] = false;
+            $data['aa'] = '';
+            $data['content'] = $this->load->view('admin/form_user', $data, true);
+            $this->load->view('admin/main', $data);
+        }
     }
 
     function add() {
-        if ($this->cek_validasi() == FALSE) {
-            $this->form();
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
         } else {
-            $data = array(
-                'USERNAME' => $this->input->post('username'),
-                'PASSWORD' => md5($this->input->post('password')),
-                'STATUS' => 1
-            );
-            $this->user_model->insert($data);
-            redirect('admin/user');
+            if ($this->cek_validasi() == FALSE) {
+                $this->form();
+            } else {
+                $data = array(
+                    'USERNAME' => $this->input->post('username'),
+                    'PASSWORD' => md5($this->input->post('password')),
+                    'STATUS' => 1
+                );
+                $this->user_model->insert($data);
+                redirect('admin/user');
+            }
         }
     }
 
     public function edit($id) {
-        $data['status'] = 'edit';
-        $record_artikel = $this->user_model->selectone($id);
-        foreach ($record_artikel->result() as $artikel) {
-            $data['id_user'] = $artikel->id_user;
-            $data['username'] = $artikel->username;
-            $data['password'] = $artikel->password;
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
+        } else {
+            $data['status'] = 'edit';
+            $record_artikel = $this->user_model->selectone($id);
+            foreach ($record_artikel->result() as $artikel) {
+                $data['id_user'] = $artikel->id_user;
+                $data['username'] = $artikel->username;
+                $data['password'] = $artikel->password;
+            }
+            //$data['status'] = 'new';
+            $data['failed'] = false;
+            $data['aa'] = '';
+            $data['content'] = $this->load->view('admin/form_user', $data, true);
+            $this->load->view('admin/main', $data);
         }
-        //$data['status'] = 'new';
-        $data['failed'] = false;
-        $data['aa'] = '';
-        $data['content'] = $this->load->view('admin/form_user', $data, true);
-        $this->load->view('admin/main', $data);
     }
 
     function update($id) {
-        //get password lama
-        $record_artikel = $this->user_model->selectone($id);
-        foreach ($record_artikel->result() as $artikel) {
-            $data_lama['password'] = $artikel->password;
-        }
-        //cek password lama
-        if ($data_lama['password'] != md5($this->input->post('konfirm_password'))) {
-            $this->edit($id);
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
         } else {
-            $data = array(
-                'USERNAME' => $this->input->post('username'),
-                'PASSWORD' => md5($this->input->post('password'))
-            );
-            $this->user_model->update($id, $data);
-            redirect('admin/user');
+            //get password lama
+            $record_artikel = $this->user_model->selectone($id);
+            foreach ($record_artikel->result() as $artikel) {
+                $data_lama['password'] = $artikel->password;
+            }
+            //cek password lama
+            if ($data_lama['password'] != md5($this->input->post('konfirm_password'))) {
+                $this->edit($id);
+            } else {
+                $data = array(
+                    'USERNAME' => $this->input->post('username'),
+                    'PASSWORD' => md5($this->input->post('password'))
+                );
+                $this->user_model->update($id, $data);
+                redirect('admin/user');
+            }
         }
     }
 
-    
     function delete() {
-        $spt_ids_post_array = split(",", $this->input->post('items'));
-        //$this->load->model('pengolahan/data_model');
-        //$msg='akun berhasil dihapus';
-        foreach ($spt_ids_post_array as $index => $id) {
-            if (isset($id) && $id != '') {
-                $this->user_model->delete($id);
-            }//end if
-        }//end foreach
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
+        } else {
+            $spt_ids_post_array = split(",", $this->input->post('items'));
+            //$this->load->model('pengolahan/data_model');
+            //$msg='akun berhasil dihapus';
+            foreach ($spt_ids_post_array as $index => $id) {
+                if (isset($id) && $id != '') {
+                    $this->user_model->delete($id);
+                }//end if
+            }//end foreach
+        }
     }
-
-    
 
     function cek_validasi() {
         // Setting Rules
@@ -189,11 +210,15 @@ class User extends CI_Controller {
     }
 
     function cek_password($value) {
-        if ($value != $this->input->post('password_konfirm')) {
-            $this->form_validation->set_message('cek_password', 'Password tidak sesuai');
-            return FALSE;
+        if ($this->session->userdata('username') == NULL) {
+            redirect('login');
         } else {
-            return TRUE;
+            if ($value != $this->input->post('password_konfirm')) {
+                $this->form_validation->set_message('cek_password', 'Password tidak sesuai');
+                return FALSE;
+            } else {
+                return TRUE;
+            }
         }
     }
 
